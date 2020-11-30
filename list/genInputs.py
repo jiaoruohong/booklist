9fcfe23fee1f6ecd4d697866601632cf60fa5310
jiaoruohong <jrh108@163.com>
2020-11-30 13:40:37 +0800

    modify genInputs.py

--- list/genInputs.py
+++ list/genInputs.py
@@ -4,17 +4,13 @@ from os.path import isfile, join
 path2dir = "./parts"
 
 if __name__ == "__main__":
-#     for f in listdir(path2dir):
-#         print(join(path2dir,f))
-#         if isfile(join(path2dir,f)):
-#             print(f)
 
-    myFiles = [f for f in listdir(path2dir) if isfile(join(path2dir, f))]
+    books = [f for f in listdir(path2dir) if isfile(join(path2dir, f))]
 
-    myFiles.sort()
-    myFiles = [join(path2dir,f) for f in myFiles]
+    books.sort()
+    books = [join(path2dir, f) for f in books]
 
     with open("./assets/files/inputs.tex", "w") as fopt:
-        for f in myFiles:
-            fopt.write("\\input{" + f + "}")
+        for f in books:
+            fopt.write("\\input{" + f + "}"+"\n")
             print("\\input{" + f + "}")
