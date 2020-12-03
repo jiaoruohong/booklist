965da129c23b0ccc662da0c860850d088424bc43
timber-bean <624322575@qq.com>
2020-12-03 10:37:27 +0800

    New part

--- list/genInputs.py
+++ list/genInputs.py
@@ -2,15 +2,17 @@ from os import listdir
 from os.path import isfile, join
 
 path2dir = "./part"
+pathSplit="/"
 
 if __name__ == "__main__":
 
-    books = [f for f in listdir(path2dir) if isfile(join(path2dir, f))]
-
+    books = [f for f in listdir(path2dir) if isfile("/".join([path2dir, f]))]
+    
     books.sort()
-    books = [join(path2dir, f) for f in books]
+    books = ["/".join([path2dir, f]) for f in books]
+    print(books)
 
-    with open("./assets/files/inputs.tex", "w") as fopt:
+    with open("./assets/files/inputs.tex", "w", encoding="utf8") as fopt:
         for f in books:
             fopt.write("\\input{" + f + "}"+"\n")
             print("\\input{" + f + "}")
