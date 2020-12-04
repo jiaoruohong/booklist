9343f8a21113276ccab7091da8ce14e5db50bdd1
jiaoruohong <jrh108@163.com>
2020-12-04 17:39:33 +0800

    add 2020.11 books; modify genInputs.py

--- list/genInputs.py
+++ list/genInputs.py
@@ -6,10 +6,10 @@ pathSplit="/"
 
 if __name__ == "__main__":
 
-    books = [f for f in listdir(path2dir) if isfile("/".join([path2dir, f]))]
+    books = [f for f in listdir(path2dir) if isfile(pathSplit.join([path2dir, f]))]
     
     books.sort()
-    books = ["/".join([path2dir, f]) for f in books]
+    books = [pathSplit.join([path2dir, f]) for f in books]
     print(books)
 
     with open("./assets/files/inputs.tex", "w", encoding="utf8") as fopt:
